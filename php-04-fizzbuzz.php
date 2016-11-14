26f1c95f2a9a1b44f26a9b8d2e5ce3fb3e514659
Anders D. Johnson <adjohnson916@users.noreply.github.com>
2016-11-13 22:14:11 -0600

    fix: 04

--- php-04-fizzbuzz.php
+++ php-04-fizzbuzz.php
@@ -1,9 +1,17 @@
 <?php
 
 /*
-	Print the numbers from 1 to 100. But for multiples of three print “Fizz” 
-	instead of the number and for the multiples of five print “Buzz”. For 
+	Print the numbers from 1 to 100. But for multiples of three print “Fizz”
+	instead of the number and for the multiples of five print “Buzz”. For
 	numbers which are multiples of both three and five print “FizzBuzz”."
 */
 
-	
+foreach (range(1,100) as $i) {
+	$t = $i % 3 == 0;
+	$f = $i % 5 == 0;
+	if ($t & $f) echo 'FizzBuzz';
+	else if ($t) echo 'Fizz';
+	else if ($f) echo 'Buzz';
+	else echo $i;
+	echo "\n";
+}

b668a3f5a30d362622b067fed61c395a55e6e378
Anders D. Johnson <adjohnson916@users.noreply.github.com>
2016-11-13 22:51:01 -0600

    fix: 05

--- php-05-hatpick.php
+++ php-05-hatpick.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-  Write a routine to match individuals from the array people against each other 
+  Write a routine to match individuals from the array people against each other
   ala Secret Santa.
 
   Note that no person should be paired with someone they "dislike"
@@ -34,3 +34,23 @@ $people = array(
   ),
 );
 
+function all_match($people) {
+  foreach ($people as $p) {
+    if (empty($p['match'])) return false;
+  }
+  return true;
+}
+
+while (!all_match($people)) {
+  foreach ($people as $n => &$p) {
+    foreach ($people as $mn => &$mp) {
+      if (!empty($mp['match'])) continue;
+      if ($p['dislikes'] == $mn) continue;
+      if ($mp['dislikes'] == $n) continue;
+      $p['match'] = $mn;
+      $mp['match'] = $n;
+    }
+  }
+}
+
+var_dump($people);

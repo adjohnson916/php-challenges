e753e521ce2897f27cf955a864a192eea44f0909
Anders D. Johnson <adjohnson916@users.noreply.github.com>
2016-11-13 23:01:40 -0600

    fix: 06

--- php-06-router.php
+++ php-06-router.php
@@ -2,10 +2,38 @@
 
 /*
   Write a simple HTTP router which accepts a URL and attempts to match it to
-  a class mapped against the URL, with a method matching the HTTP verb 
+  a class mapped against the URL, with a method matching the HTTP verb
   (GET, POST, etc.)
 
   Non mapped URLs should result in a 404 response being returned.
 
   Feel free to use more that one file if needed.
 */
+
+class Router {
+  private $routes = [];
+
+  public function register ($path, $class, $method) {
+    $this->routes[$path] = [
+      'class' => $class,
+      'method' => $method
+    ];
+  }
+
+  public function route ($url) {
+    $route = $this->routes[$url];
+    if (empty($route)) return "404\n";
+    return $route['class']::$route['method']();
+  }
+
+}
+
+class Hello {
+  public static function get() {
+    return "hello\n";
+  }
+}
+
+$router = new Router;
+$router->register('/hello', 'Hello', 'get');
+echo $router->route('/hello');
